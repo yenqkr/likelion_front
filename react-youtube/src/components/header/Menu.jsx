82dec74e0743bae12d53e6b8d184106c044d9b63
yenqkr <sksksk951@naver.com>
2024-02-17 05:12:20 +0900

    [fix] header area componentization

---
+++ react-youtube/src/components/header/Menu.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { headerMenus, searchKeyword } from "../../data/header";
+import { Link, useLocation } from "react-router-dom";
+
+const Menu = () => {
+  const location = useLocation();
+  return (
+    <nav className="header__menu">
+      <ul className="menu">
+        {headerMenus.map((menu, key) => (
+          <li
+            key={key}
+            // location.pathname === menu.src 같으면 active 붙이고 아니면 ""
+            className={location.pathname === menu.src ? "active" : ""}
+          >
+            <Link to={menu.src}>
+              {menu.icon}
+              {menu.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <ul className="keyword">
+        {searchKeyword.map((keyword, key) => (
+          <li
+            key={key}
+            className={location.pathname === keyword.src ? "active" : ""}
+          >
+            <Link to={keyword.src}>{keyword.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Menu;
