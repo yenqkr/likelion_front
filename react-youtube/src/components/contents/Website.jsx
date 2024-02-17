58b59a06ed696cfe09093d31189753d4cd9f9007
yenqkr <sksksk951@naver.com>
2024-02-17 15:55:47 +0900

    [fix] rest of main section

--- react-youtube/src/components/contents/Website.jsx
+++ react-youtube/src/components/contents/Website.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+import { websiteText } from "../../data/website";
+import { Link } from "react-router-dom";
+
 const Website = () => {
-  return <div>Website</div>;
+  return (
+    <section id="website">
+      <h2>😛 웹표준 사이트 만들기 기초 다지기</h2>
+      <div className="video__inner">
+        {websiteText.map((video, key) => (
+          <div className="video" key={key}>
+            <div className="video__thumb play__icon">
+              <Link to={`/video/${video.videoId}`}>
+                <img src={video.img} alt={video.title} />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Website;
